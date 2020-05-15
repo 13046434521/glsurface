5056a2c13d0fc178e63a22c4e985d2169f1a639d
贾天龙 <604872657@qq.com>
2020-05-15 14:13:40 +0800

    新增常用参数类

---
+++ glsurface/src/main/java/com/jtl/surface/Constants.java
@@ -0,0 +1,10 @@
+package com.jtl.surface;
+
+/**
+ * @author：TianLong
+ * @date：2020/5/15 14:04
+ * 常用参数
+ */
+public class Constants {
+    public String VERSION_NAME = BuildConfig.VERSION_NAME;
+}
